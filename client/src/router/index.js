76be905f769d56e39869018f02af49eebe388f94
Kate Birmingham <kabi6686@colorado.edu>
2018-08-20 14:53:10 -0600

    changed to history mode instead of hash vue-router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Posts from '@/components/Posts'
 import Home from '@/components/Home'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
