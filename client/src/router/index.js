040264174f5d9496f44ebb91595426f0c121efdb
Kate Birmingham <kabi6686@colorado.edu>
2018-09-05 11:30:02 -0600

    posts component/endpoint with GET request from backend server

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Register from '@/components/Register'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: Posts
     }
   ]
 })
