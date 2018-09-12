d55e03d239f7ff42fab7b52fd2ae81ddd606113d
Kate Birmingham <kabi6686@colorado.edu>
2018-09-12 15:55:41 -0600

    connection to posts database in mongo via mongoose with post route in app.js, template for ES6 version in database.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+const Post = require('../models/post')
 
 const app = express()
 app.use(morgan('combined'))
@@ -23,6 +25,33 @@ app.get('/posts', (req, res) => {
       description: "Hi there! How are you?"
     }]
   )
+})
+
+mongoose.connect('mongodb://localhost:27017/posts')
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+  console.log("Connection Succeeded");
+})
+
+app.post('/posts', (req, res) => {
+  const db = req.db
+  const title = req.body.title
+  const description = req.body.description
+  const new_post = new Post({
+    title,
+    description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
 })
 
 app.listen(process.env.PORT || 8081)
